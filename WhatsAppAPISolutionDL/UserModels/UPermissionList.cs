32ec114d92a66734729984a57630def77ee23ce9
BurhanJivanji <burhan@consulttechies.com>
2024-10-28 11:51:23 +0530

    updated  permission list

--- WhatsAppAPISolutionDL/UserModels/UPermissionList.cs
+++ WhatsAppAPISolutionDL/UserModels/UPermissionList.cs
@@ -9,12 +9,14 @@ namespace WhatsAppAPISolutionDL.UserModels
     public partial class UPermissionList
     {
         public long PermissionId { get; set; }
+        public int Module { get; set; }
+        public string ModuleName { get; set; }
+        public int PermissionTaskId { get; set; }
+        public string PermissionTaskName { get; set; }
+        public int? ParentId { get; set; }
+        public bool? Can_View { get; set; }
         public bool? Can_Create { get; set; }
         public bool? Can_Update { get; set; }
         public bool? Can_Delete { get; set; }
-        public int PermissionTaskId { get; set; }
-        public string PermissionTaskName { get; set; }
-        public int Module { get; set; }
-        public int? ParentId { get; set; }
     }
 }
