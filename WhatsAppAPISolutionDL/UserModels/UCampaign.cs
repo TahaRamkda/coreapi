ce0d431d4a9c649ead0bb5b9c2b2b335a9f82bbc
BurhanJivanji <burhan@consulttechies.com>
2024-12-04 12:04:45 +0530

    update campaign page

--- WhatsAppAPISolutionDL/UserModels/UCampaign.cs
+++ WhatsAppAPISolutionDL/UserModels/UCampaign.cs
@@ -14,7 +14,7 @@ namespace WhatsAppAPISolutionDL.UserModels
         public int SenderId { get; set; }
         public int TemplateId { get; set; }
         public string ScheduleDate { get; set; }
-        public string Status { get; set; }
+        public int? Status { get; set; }
         public int? TotalContacts { get; set; }
         public int? SentCount { get; set; }
         public int? FailedCount { get; set; }
