074f69df10f70da237708cdc96db8540c6586fa3
BurhanJivanji <burhan@consulttechies.com>
2025-02-10 15:57:39 +0530

    added signalR configuration-1

--- WhatsAppAPISolutionAPI/Program.cs
+++ WhatsAppAPISolutionAPI/Program.cs
@@ -21,7 +21,6 @@ builder.Services.AddApplicationServices(configuration);
 builder.Services.AddDatabaseServices(configuration);
 builder.Services.AddSettingServices(configuration);
 builder.Services.AddHttpClientServices(configuration);
-builder.Services.AddOneSignalServices(configuration);
 
 // Adding Authentication
 builder.Services.AddAuthentication(options =>
