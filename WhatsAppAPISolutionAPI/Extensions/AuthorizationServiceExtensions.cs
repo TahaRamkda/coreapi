172fb75ad3bc640df665981118482cc5be7a116b
Huzeifa Ambua <huzeifa.a@koutfood.com>
2025-04-14 10:27:41 +0300

    Huzeifa - program.cs cleanup

---
+++ WhatsAppAPISolutionAPI/Extensions/AuthorizationServiceExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Tokens;
+using WhatsAppAPISolutionAPI.Middleware;
+using WhatsAppAPISolutionDL.Setting;
+using System.Text;
+
+namespace WhatsAppAPISolutionAPI.Extensions
+{
+    public static class AuthorizationServiceExtensions
+    {
+        public static IServiceCollection AddAuthorizationServices(this IServiceCollection services, IConfiguration config)
+        {
+            // Adding Authentication
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ClockSkew = TimeSpan.Zero,
+                    ValidAudience = config["Jwt:Audience"],
+                    ValidIssuer = config["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]))
+                };
+            })
+            .AddScheme<AuthenticationSchemeOptions, ApiKeyAuthenticationHandler>("ApiKey", null);
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy(AuthenticationSchemes.ApiKeyPolicy, policy => policy.RequireAuthenticatedUser().AddAuthenticationSchemes("ApiKey"));
+                options.AddPolicy(AuthenticationSchemes.BearerPolicy, policy => policy.RequireAuthenticatedUser().AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme));
+            });
+
+            return services;
+        }
+    }
+}
---
+++ WhatsAppAPISolutionAPI/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+using WhatsAppAPISolutionAPI.Helper;
+
+namespace WhatsAppAPISolutionAPI.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerServices(this IServiceCollection services)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+
+            //services.AddSwaggerGen(setup =>
+            //{
+            //    var schemaHelper = new SwashbuckleSchemaHelper();
+            //    setup.CustomSchemaIds(type => schemaHelper.GetSchemaId(type));
+
+            //    // Include 'SecurityScheme' to use JWT Authentication
+            //    var jwtSecurityScheme = new OpenApiSecurityScheme
+            //    {
+            //        BearerFormat = "JWT",
+            //        Name = "JWT Authentication",
+            //        In = ParameterLocation.Header,
+            //        Type = SecuritySchemeType.Http,
+            //        Scheme = JwtBearerDefaults.AuthenticationScheme,
+            //        Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
+
+            //        Reference = new OpenApiReference
+            //        {
+            //            Id = JwtBearerDefaults.AuthenticationScheme,
+            //            Type = ReferenceType.SecurityScheme
+            //        }
+            //    };
+
+            //    setup.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+            //    setup.AddSecurityRequirement(new OpenApiSecurityRequirement { { jwtSecurityScheme, Array.Empty<string>() } });
+
+            //});
+
+            services.AddSwaggerGen(setup =>
+            { 
+                var schemaHelper = new SwashbuckleSchemaHelper();
+                setup.CustomSchemaIds(type => schemaHelper.GetSchemaId(type));
+
+                setup.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter the Bearer token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+
+                setup.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter the API key",
+                    Name = "X-API-KEY",
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    },
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "ApiKey"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+
+            return services;
+        }
+    }
+}
