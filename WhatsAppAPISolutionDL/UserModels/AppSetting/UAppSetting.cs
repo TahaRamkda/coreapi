deabf42f03c2b5a96efa9aec05ace75e5ad832bb
Shabbir Bohra <shabbir@consulttechies.com>
2025-04-18 13:05:21 +0530

    shabbir - Added TotalRecord in the listing in the appsettings

--- WhatsAppAPISolutionDL/UserModels/AppSetting/UAppSetting.cs
+++ WhatsAppAPISolutionDL/UserModels/AppSetting/UAppSetting.cs
@@ -13,5 +13,6 @@ namespace WhatsAppAPISolutionDL.UserModels.AppSetting
         public string Val { get; set; }
         public int? ClientId { get; set; }
         public int? SenderId { get; set; }
+        public int? TotalRecords { get; set; }
     }
 }
