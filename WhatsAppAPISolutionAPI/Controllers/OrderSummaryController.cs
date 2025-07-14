5ba3d5d9170ea3608057a77b4ffb4fa189d3fe89
Shabbir Bohra <shabbir@consulttechies.com>
2025-07-14 11:08:53 +0530

    shabbir - Changed the naming of the ordersummarycontroller

--- WhatsAppAPISolutionAPI/Controllers/OrderSummaryController.cs
+++ WhatsAppAPISolutionAPI/Controllers/OrderSummaryController.cs
@@ -8,20 +8,20 @@ namespace WhatsAppAPISolutionAPI.Controllers
     [Route("[controller]")]
     [ApiController]
     [Authorize]
-    public class OrderSummaryDetailsController : ControllerBase
+    public class OrderSummaryController : ControllerBase
     {
         private readonly IOrderSummaryDetailsService _OrderSummyDetailsService;
         private readonly int clientId;
         private readonly IUserService _userService;
 
-        public OrderSummaryDetailsController(IOrderSummaryDetailsService OrderSummyDetailsService, IUserService userService)
+        public OrderSummaryController(IOrderSummaryDetailsService OrderSummyDetailsService, IUserService userService)
         {
             _OrderSummyDetailsService = OrderSummyDetailsService;
             _userService = userService;
             clientId = _userService.GetClientIdFromAccessToken();
         }
 
-        [HttpGet("OrderSummaryList")]
+        [HttpGet("ordersummarylist")]
         public async Task<IActionResult> GetOrderSummaries(int senderId, int pageNo = 1,  int pageSize = 20,  string searchStr = "")
         {
             var result = await _OrderSummyDetailsService.GetOrderSummariesAsync(clientId, senderId, pageNo, pageSize, searchStr);
@@ -33,7 +33,7 @@ namespace WhatsAppAPISolutionAPI.Controllers
             });
         }
 
-        [HttpGet("OrderDetails")]
+        [HttpGet("ordersummarydetails")]
         public async Task<IActionResult> GetOrderDetails(int orderId)
         {
             var result = await _OrderSummyDetailsService.GetOrderDetailsAsync(orderId);
